c1d19464ca8d6346258fb7b573ea362d04223027
Marton Adrienn <marton.adrienn@outlook.hu>
2022-09-03 14:57:23 +0200

    Mindenképp váltson

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 let imgs = ["image1.jpg", "image2.jpg", "image3.jpg"];
+let old_img = -1;
 
 setTimeout(ChangeImg, 3000);
 
 function ChangeImg() {
-    document.getElementById('image').src = imgs[Math.floor(Math.random() * 3)];
+    do {
+        new_img = Math.floor(Math.random() * 3);
+    } while( new_img == old_img);
+    old_img = new_img;
+    document.getElementById('image').src = imgs[new_img];
     setTimeout(ChangeImg, 2000);
 }
