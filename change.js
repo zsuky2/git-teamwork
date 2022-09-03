dabb8daaa65a078c95506a81d37d093859c3a1a4
Marton Adrienn <marton.adrienn@outlook.hu>
2022-09-03 14:45:17 +0200

    js javítás

--- change.js
+++ change.js
@@ -1,11 +1,8 @@
 let imgs = ["image1.jpg", "image2.jpg", "image3.jpg"];
-let act = 0;
 
-setTimeout(ChangeImg, 3000);
+setTimeout(ChangeImg, 2000);
 
 function ChangeImg() {
     document.getElementById('image').src = imgs[Math.floor(Math.random() * 3)];
-    // if (act >= 3) act = 0;
-
-    setTimeout(ChangeImg, 3000);
+    setTimeout(ChangeImg, 2000);
 }
