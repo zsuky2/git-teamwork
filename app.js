3163f2c8945edf067a9c9bd8e040fc114accf539
zs-eszter <zhukovsky.esther@gmail.com>
2022-09-03 15:18:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 let imgs = ["image1.jpg", "image2.jpg", "image3.jpg"];
-let act = 0;
 
-setTimeout(ChangeImg, 2000);
 
 function ChangeImg() {
     document.getElementById('image').src = imgs[Math.floor(Math.random() * 3)];
-    // if (act >= 3) act = 0;
 
     setTimeout(ChangeImg, 2000);
 }
+
+ChangeImg();
