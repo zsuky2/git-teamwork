ca3e5077f76d82eeda80504685edc8c79f9a1d4c
zs-eszter <zhukovsky.esther@gmail.com>
2022-09-03 14:46:08 +0200

    fájlok módosítása megtörtént

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-document.getElementById("btn").addEventListener("click", function () {
+let imgs = ["image1.jpg", "image2.jpg", "image3.jpg"];
+let act = 0;
 
-    
-    
-    var i = Math.floor(Math.random()*3)+1;;
-    
-    
-    document.getElementById("result").innerHTML=i;
+setTimeout(ChangeImg, 3000);
 
+function ChangeImg() {
+    document.getElementById('image').src = imgs[Math.floor(Math.random() * 3)];
+    // if (act >= 3) act = 0;
 
-})
+    setTimeout(ChangeImg, 2000);
+}
