2b0561a458b383a8b5245e803d02317595e0a7d8
zs-eszter <zhukovsky.esther@gmail.com>
2022-09-03 14:24:22 +0200

    index.html szerkesztve

---
+++ app.js
@@ -0,0 +1,11 @@
+document.getElementById("btn").addEventListener("click", function () {
+
+    
+    
+    var i = Math.floor(Math.random()*3)+1;;
+    
+    
+    document.getElementById("result").innerHTML=i;
+
+
+})
